f20a1115400d811e017987b433681be10d95556c
Divine Abraham <divineabraham007@gmail.com>
2016-10-25 19:33:02 +0100

    ex1

--- sprint2/SpringMvc_ex01/src/main/java/labMVC/IndexController.java
+++ sprint2/SpringMvc_ex01/src/main/java/labMVC/IndexController.java
@@ -8,8 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
-	
-	// ADD YOUR REQUEST MAPPING AND METHOD HERE
-	
+public class IndexController 
+{
+	@RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "World");
+        return "hello";
+    }
 }
