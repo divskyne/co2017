f55af849244b212abdff47891cd2795b8949937f
Divine Abraham <divineabraham007@gmail.com>
2016-11-07 18:36:59 +0000

    mini-project merge

--- sprint2/mini-project/src/main/java/eMarket/EMarketApp.java
+++ sprint2/mini-project/src/main/java/eMarket/EMarketApp.java
@@ -1,15 +1,22 @@
-package miniProject;
+package eMarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import eMarket.domain.Store;
+
 @SpringBootApplication
-public class miniProjectApplication extends WebMvcConfigurerAdapter {
+public class EMarketApp extends WebMvcConfigurerAdapter {
 
+	private static Store store = new Store();
+	
     public static void main(String[] args) {
-        SpringApplication.run(miniProjectApplication.class, args);
+        SpringApplication.run(EMarketApp.class, args);
     }
+
+	public static Store getStore() {
+		return store;
+	}
+
 }
-
-
