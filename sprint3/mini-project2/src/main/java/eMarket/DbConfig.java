86bf118642b1b7b63ada9924280ff1a88f36c96c
Divine Abraham <divineabraham007@gmail.com>
2016-12-06 15:43:56 +0000

    perfect code copied

--- sprint3/mini-project2/src/main/java/eMarket/DbConfig.java
+++ sprint3/mini-project2/src/main/java/eMarket/DbConfig.java
@@ -12,17 +12,17 @@ public class DbConfig {
 
 	// information about accessing the Departmental MySQL server
 	// https://campus.cs.le.ac.uk/labsupport/usinglinux/mysqlaccountdetails
-	private String USERNAME = "abc123";
-	private String PASSWORD = "password"; // in ~/.my.cnf
+	private String USERNAME = "da201";
+	private String PASSWORD = "iezauthi"; // in ~/.my.cnf
 	
 	// from campus
-	private String HOST = "mysql.mcscw3.le.ac.uk";
-	private int PORT = 3306;
+//	private String HOST = "mysql.mcscw3.le.ac.uk";
+//	private int PORT = 3306;
 	
 	// off-campus: after executing 
 	// ssh -fNg -L 3307:mysql.mcscw3.le.ac.uk:3306 abc123@xanthus.mcscw3.le.ac.uk
-//	private String HOST = "127.0.0.1";
-//	private int PORT = 3307;
+	private String HOST = "127.0.0.1";
+	private int PORT = 3306;
 			
     @Bean
     public DriverManagerDataSource dataSource() {		
